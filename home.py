e23faba02e80837aa0b644caf6aecf459be4f7ac
Vito <vito.pramudita1502@gmail.com>
2022-05-24 13:51:42 +0700

    Changed description in home

--- home.py
+++ home.py
@@ -3,16 +3,14 @@ import streamlit as st
 def description():
     st.header("👋Welcome to our resume parser!")
     st.write(""" 
-Dicta voluptatem laudantium ut fugiat quo porro aperiam facere. Quo molestiae neque mollitia. Voluptatem aut enim est sit velit.
+### This website is to extract and parse your Resume document
 
-Repellendus molestias est est aliquam. Quis distinctio quam quia. Id beatae in omnis ut eos.
-
-Cum inventore possimus id est quas non assumenda omnis. Molestiae placeat non aut sequi quisquam rerum non. Itaque ut molestias odit labore.
-
-Sequi molestiae tempore optio error et. Saepe distinctio vitae officiis odio fugit. Rerum quo temporibus dolorem totam sed.
-
-Aut aut adipisci dolor. Non aut ipsam tenetur. Dolor eum nostrum aut nostrum dolore. Aut dicta id veritatis. Culpa aliquid beatae voluptate voluptates voluptates consequatur tempora assumenda. Sequi esse commodi temporibus fuga aliquid sit fugit.
-
+#### How to use this application:
+1. Choose your Resume document.
+2. Go to Parse my Resume menu.
+3. Upload the document you have chosen. Please make sure your Resume document is in PDF format.
+4. Drop your file or drag and drop it.
+5. Voila, your PDF has been parsed!
     """)
 
 def about():
